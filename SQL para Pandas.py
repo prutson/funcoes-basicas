61419114e7283481883ac82d38675c9f2d495fb2
João Pereira <91325388+prutson@users.noreply.github.com>
2023-03-30 21:49:24 -0300

    Update SQL para Pandas.py

--- SQL para Pandas.py
+++ SQL para Pandas.py
@@ -8,9 +8,17 @@ df_union = pd.concat([df1. df2]).reset_index(drop=True)
 #select * from df1 1 inner join df2 2 on 1.key = 2.key
 df_join = df1.merge(df2, on = 'key', suffixes=('_1', '_2'))
 
-#Group By
+# Inner join com duas clausulas 
+#select * from df1 1 inner join df2 2 on 1.key1 = 2.key1 and 1.key2 = 2.key2
+df_join = df1.merge(df2, on = ['key1', 'key2'], suffixes=('_1', '_2'))
+
+# Count Group By
 #select var1, count(var2) grupo by var1
 counted_df = df.groupby('var1').agg({'var2':'count'})
+
+# AVG Group By
+#select var1, count(var2) grupo by var1
+counted_df = df.groupby('var1').agg({'var2':'median'})
 
 # Order by
 #select va1 order by var1 desc
