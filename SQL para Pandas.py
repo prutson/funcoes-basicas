08881fdb339f15312c0c1e929e0f787984dba936
João Pereira <91325388+prutson@users.noreply.github.com>
2023-03-30 21:26:30 -0300

    Update SQL para Pandas.py

--- SQL para Pandas.py
+++ SQL para Pandas.py
@@ -5,3 +5,12 @@ df_union = pd.concat([df1. df2]).reset_index(drop=True)
 
 #Inner join
 df_join = df1.merge(df2, on = 'key')
+
+#One to many
+df_join = df1.merge(df2, on = 'key', suffixes=('_key', '_key'))
+
+#select var1, count(var2) grupo by var1
+counted_df = df.groupby('var1').agg({'var2':'count'})
+
+#select va1 order by var1 desc
+sorted_df = df.sort_values('var1', ascending = False)
